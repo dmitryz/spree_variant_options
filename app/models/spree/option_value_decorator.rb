1079a73c8396a218fa7c1411eb772cc49c2ec756
Dmitry <dmitry@yulchik.com>
2014-08-10 09:25:24 +0300

    add validator

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -6,6 +6,11 @@ Spree::OptionValue.class_eval do
     :url           => SpreeVariantOptions::VariantConfig[:option_value_url],
     :path          => SpreeVariantOptions::VariantConfig[:option_value_path]
 
+	validates_attachment_content_type :image, :content_type => [/\Aimage/, /\Aaudio/]
+
+	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /mp3\Z/]
+
+
   def has_image?
     image_file_name && !image_file_name.empty?
   end
